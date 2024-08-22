c3ad243c0551e1ea10bcc8b5604f7bf44f664c10
Kekyra228 <katyiv02@gmail.com>
2024-08-22 15:17:41 +0400

    Express и MongoDB

--- src/app.js
+++ src/app.js
@@ -1,56 +1,52 @@
-// const http = require("http");
-// const PORT = 3000;
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/urers");
+const bookRouter = require("./routes/books");
+const notFound = require("./middlewares/notFound");
+const originalUrl = require("./middlewares/originalUrl");
 
-// const getUsers = require("./module/users");
-// const greeting = require("./module/hello");
+dotenv.config();
 
-// const server = http.createServer((req, res) => {
-//   console.log("server");
-//   if (req.url === "/users") {
-//     res.status = 200;
-//     res.statusMessage = "OK";
-//     res.header = "Content-Type: application/json";
-//     res.write(getUsers());
-//     res.end();
-//     return;
-//   }
-//   res.status = 200;
-//   res.statusMessage = "OK";
-//   res.header = "Content-Type: text/plain";
-//   res.write(greeting);
-//   res.end();
-// });
-
-// server.listen(PORT, "localhost", () => {
-//   console.log(`Сервер запущен ${PORT}`);
-// });
-
-import express from "express";
-import http from "http";
-import usersJson from "./data/users.json" with {type: "json"}
-
-const PORT = 3000;
 const app = express();
+const { PORT, API_URL, MONGO_URL } = process.env;
 
-app.get("/", (req, res) => {
-  const name = req.query.hello;
-  const users = req.query.users;
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Conneted to MongoDB"))
+  .catch((error) => console.log(error));
 
-  if (name === "") {
-    res.status(400).send("Enter a name");
-  } else if (name) {
-    res.status(200).send(`Hello, ${name}`);
-  } else if (users === "") {
-     res.status(200).json(usersJson);
-  }
-  if (Object.keys(req.query).length) {
-    res.status(500).send("");
-  }
-  res.status(200).send("Hello, world");
+app.use(cors());
+app.use(bodyparser.json());
+app.use(originalUrl);
+app.use(userRouter);
+app.use(bookRouter);
+app.use(notFound);
+
+app.listen(PORT, () => {
+  console.log(`Север запущен по адресу ${API_URL}:${PORT}`);
 });
+// app.get("/", (req, res) => {
+//   const name = req.query.hello;
+//   const users = req.query.users;
 
-const server = http.createServer(app);
+//   if (name === "") {
+//     res.status(400).send("Enter a name");
+//   } else if (name) {
+//     res.status(200).send(`Hello, ${name}`);
+//   } else if (users === "") {
+//     res.status(200).json(usersJson);
+//   }
+//   if (Object.keys(req.query).length) {
+//     res.status(500).send("");
+//   }
+//   res.status(200).send("Hello, world");
+// });
 
-server.listen(PORT, () => {
-  console.log("server started");
-});
+// const server = http.createServer(app);
+
+// server.listen(PORT, () => {
+//   console.log("server started");
+// });
